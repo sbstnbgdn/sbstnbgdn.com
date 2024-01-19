8127d587e3348f6bfd1ab36bd45d0a9761849944
Sebastian Bogdan <hello@sebastianbogdan.com>
2024-01-19 20:22:41 +0000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://sbstnbgdn.com",
+  integrations: [tailwind(), sitemap()],
+  prefetch: true,
+  experimental: {
+    clientPrerender: true,
+  },
+});
