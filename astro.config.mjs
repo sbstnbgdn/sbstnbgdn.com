e918d534ab95f87be0fac9b4c47db857e483004b
Sebastian Bogdan <ankh-cachets0q@icloud.com>
2024-12-03 13:43:51 +0000

    chore: Update project structure and dependencies, remove unused files, and switch to strict TypeScript configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://sbstnbgdn.com",
-  integrations: [tailwind(), sitemap()],
-  prefetch: {
-    prefetchAll: true,
-  },
+  integrations: [tailwind()],
 });
